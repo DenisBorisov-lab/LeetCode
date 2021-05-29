6f0857bfe423d3eb4119cd048962e22a3e72f2c9
DenisBorisov-lab <mai.borisov.denis@gmail.com>
2021-05-29 21:50:25 +0300

    ficsed solution

--- src/main/java/SimplifyPath/SimplifyPath.java
+++ src/main/java/SimplifyPath/SimplifyPath.java
@@ -1,41 +1,29 @@
 package SimplifyPath;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class SimplifyPath {
     public String simplifyPath(String path) {
-        String[] directors = path.split("/");
-        ArrayList<String> smartDirectories = new ArrayList<>(Arrays.asList(directors));
-        StringBuilder simplePath = new StringBuilder();
-        for (String smartDirectory : smartDirectories) {
-            if (smartDirectory.equals(".") || smartDirectory.equals("")) {
-            } else {
-                simplePath.append("/").append(smartDirectory);
-            }
-        }
-        return remove(simplePath.toString());
-    }
-    public static String remove(String path){
         String[] directories = path.split("/");
-        ArrayList<String> list = new ArrayList<>();
-        for (String word : directories){
-            if (word.equals("..")){
-                if (list.size() != 0){
-                    list.remove(list.size() - 1);
+        ArrayList<String> simplePath = new ArrayList<>();
+        for (String directory : directories) {
+            if (directory.equals("") || directory.equals("/") || directory.equals(".")) {
+                continue;
+            } else if (directory.equals("..")) {
+                if (simplePath.size() == 0) {
+                    continue;
+                } else {
+                    simplePath.remove(simplePath.size() - 1);
                 }
-            }else if (word.equals("")){
-            }else {
-                list.add("/" + word);
+            } else {
+                simplePath.add("/" + directory);
             }
         }
-        StringBuilder builder = new StringBuilder();
-        for (String word : list){
-            builder.append(word);
+        String result = String.join("", simplePath);
+        if (result.length() == 0) {
+            result += "/";
         }
-        if (builder.length() == 0){
-            builder.append("/");
-        }
-        return builder.toString();
+        return result;
+
     }
 }
