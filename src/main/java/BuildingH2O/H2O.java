89cf7518ee0d7c824a0baff95f49ef67743a9418
Denis Borisov <mai.borisov.denis@gmail.com>
2021-07-30 11:14:49 +0300

    добавил решение H2O задачи

---
+++ src/main/java/BuildingH2O/H2O.java
@@ -0,0 +1,33 @@
+package BuildingH2O;
+
+import java.util.concurrent.Semaphore;
+
+public class H2O {
+
+    Semaphore sH;
+    Semaphore sO;
+    private int i;
+
+    public H2O() {
+        sH = new Semaphore(2);
+        sO = new Semaphore(0);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        sH.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        i++;
+        if (i % 2 == 0) {
+            sO.release();
+        }
+        sH.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        sO.acquire();
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        sH.release(2);
+    }
+}
